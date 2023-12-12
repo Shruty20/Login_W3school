3638dc3442f8c7b39b0caaff264a1b1a851bdd57
Shruty Aggarwal <84934515+Shruty20@users.noreply.github.com>
2023-12-12 22:40:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+chrome_option = webdriver.ChromeOptions()
+chrome_option.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_option)
+
+driver.get("https://profile.w3schools.com/log-in?redirect_url=https%3A%2F%2Fmy-learning.w3schools.com")
+Email = driver.find_element(By.NAME, value="email")
+Email.send_keys("shrutyaggarwal2002@gmail.com")
+Password = driver.find_element(By.NAME, value="current-password")
+Password.send_keys("shruschooL@20")
+submit = driver.find_element(By.CLASS_NAME, "Button_primary__d2Jt3")
+submit.click()
